710f9f334d08afbab6723be94c33bdf7d7b07c5e
Hiro <s4moozy@gmail.com>
2021-01-21 07:53:23 +0900

    added first component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <learning-resource 
+      v-for="res in storedResources" 
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Google search engine',
+          link: 'https://google.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
